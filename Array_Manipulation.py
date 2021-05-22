2ea7baedaf1899ab637ab59f3c3a58e8ebcf9d6b
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2021-05-22 20:39:19 +0530

    array manipulation added

---
+++ Array_Manipulation.py
@@ -0,0 +1,28 @@
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    c = Counter()
+    for a,b,k in queries:
+        c[a]  +=k
+        c[b+1]-=k
+    arrSum = 0
+    maxSum = 0
+    for i in sorted(c)[:-1]:
+        arrSum+= c[i]
+        maxSum = max(maxSum,arrSum)
+    return maxSum
+    
